9af6f4a4950ab9a557cee2a2979fb6b9cceafa6b
Dwight Lyle <dwightjl@gmail.com>
2023-08-01 16:03:04 -0700

    Error fixes

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,21 +1,17 @@
 /** @jsxImportSource preact */
 import styles from "./Tooltip.module.css"
 
-
-
 export type Props = {
-  description: string,
-  children: string,
+  description: string
+  children: string
 }
 
 const Tooltip = ({ description, children }: Props) => {
-
-  description = description
-
   return (
-    <span class={styles.tooltip} description={description}>{children}</span>
+    <span class={styles.tooltip} tooltip-text={description}>
+      {children}
+    </span>
   )
 }
 
 export default Tooltip
-
